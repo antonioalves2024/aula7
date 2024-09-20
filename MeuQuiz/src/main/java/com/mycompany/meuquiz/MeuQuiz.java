42c5264b17a129de551fe703792baf85344c7d01
Antonio Alves <antonio@colegiosinodaldapaz.org.br>
2024-09-19 21:37:21 -0300

    Criando o app em Java

---
+++ MeuQuiz/src/main/java/com/mycompany/meuquiz/MeuQuiz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.meuquiz;
+
+/**
+ *
+ * @author aluno
+ */
+public class MeuQuiz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
